f4cc2ebfaf68e2dd0d5e449744827d8c945f2e69
Avelino Tiu III <avelino.tiu@wavecell.com>
2017-05-08 13:23:00 +0800

    event banner

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,7 @@ bootstrap.js
 jquery.mmenu.all.min.js
 */
 $(document).ready(function() {
+  // mobile nav
   $("#mobile-menu").mmenu({
      // configuration
      searchfield: {
@@ -16,4 +17,13 @@ $(document).ready(function() {
      },
      title: ''
   });
+
+  //countdown timer in events page
+  $("#countdown-timer")
+  .countdown("2017/06/28 11:30:00", function(event) {
+    $(this).html(
+      event.strftime('<span><strong>%D</strong> days</span> <span><strong>%H</strong> hours</span> <span><strong>%M</strong> minutes</span> <span><strong>%S</strong> seconds</span>')
+    );
+  });
+
 });
